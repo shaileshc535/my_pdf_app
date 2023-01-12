fad03c2aace85f1e19338b388e9f68cfbbf3b986
shailesh chaurasiya <shaileshc535@gmail.com>
2023-01-12 18:14:39 +0530

    change

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
@@ -8,87 +8,87 @@ const TerserPlugin = require("terser-webpack-plugin");
 require("dotenv").config();
 
 module.exports = (env = {}) => {
-    const {mode = "development", context = "/"} = env;
+  const { mode = "development", context = "/" } = env;
 
-    const {API_URL} = process.env;
+  const { API_URL } = process.env;
 
-    let publicPath = context;
+  let publicPath = context;
 
-    let src = path.resolve(__dirname, "src");
+  let src = path.resolve(__dirname, "src");
 
-    let output = path.resolve(__dirname, "dist");
+  let output = path.resolve(__dirname, "build");
 
-    let is_development = mode === "development";
+  let is_development = mode === "development";
 
-    return {
-        mode,
-        devtool: is_development ? "source-map" : false,
-        entry: [path.resolve(src, "index.ts"), path.resolve(src, "index.scss")],
-        output: {
-            path: output,
-            publicPath,
+  return {
+    mode,
+    devtool: is_development ? "source-map" : false,
+    entry: [path.resolve(src, "index.ts"), path.resolve(src, "index.scss")],
+    output: {
+      path: output,
+      publicPath,
+    },
+    optimization: {
+      runtimeChunk: "single",
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /node_modules/,
+            name: "vendor",
+            enforce: true,
+            chunks: "initial",
+          },
         },
-        optimization: {
-            runtimeChunk: "single",
-            splitChunks: {
-                cacheGroups: {
-                    vendor: {
-                        test: /node_modules/,
-                        name: "vendor",
-                        enforce: true,
-                        chunks: "initial",
-                    },
-                },
+      },
+      minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin({})],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts(x?)$/,
+          use: [
+            {
+              loader: "ts-loader",
+              options: {
+                transpileOnly: true,
+                experimentalWatchApi: true,
+              },
             },
-            minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin({})],
+          ],
         },
-        module: {
-            rules: [
-                {
-                    test: /\.ts(x?)$/,
-                    use: [
-                        {
-                            loader: "ts-loader",
-                            options: {
-                                transpileOnly: true,
-                                experimentalWatchApi: true,
-                            },
-                        },
-                    ],
-                },
-                {
-                    test: /index\.scss$/,
-                    use: [MiniCssExtractPlugin.loader, "css-loader"],
-                },
-                {
-                    test: /\.scss$/,
-                    use: ["sass-loader"],
-                },
-                {
-                    test: /\.(svg|jpg|png|jpeg|gif|eot|woff|ttf|ico)/,
-                    use: "file-loader",
-                },
-            ],
+        {
+          test: /index\.scss$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
         },
-        resolve: {
-            extensions: [".js", ".json", ".ts", ".tsx"],
+        {
+          test: /\.scss$/,
+          use: ["sass-loader"],
         },
-        devServer: {
-            contentBase: output,
-            writeToDisk: true,
+        {
+          test: /\.(svg|jpg|png|jpeg|gif|eot|woff|ttf|ico)/,
+          use: "file-loader",
         },
-        plugins: [
-            new CleanWebpackPlugin(),
-            new webpack.DefinePlugin({
-                env: JSON.stringify({API_URL}),
-            }),
-            new MiniCssExtractPlugin({
-                chunkFilename: "[name].css",
-            }),
-            new HtmlWebpackPlugin({
-                hash: true,
-                template: "./src/index.ejs",
-            }),
-        ],
-    };
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".json", ".ts", ".tsx"],
+    },
+    devServer: {
+      contentBase: output,
+      writeToDisk: true,
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new webpack.DefinePlugin({
+        env: JSON.stringify({ API_URL }),
+      }),
+      new MiniCssExtractPlugin({
+        chunkFilename: "[name].css",
+      }),
+      new HtmlWebpackPlugin({
+        hash: true,
+        template: "./src/index.ejs",
+      }),
+    ],
+  };
 };
